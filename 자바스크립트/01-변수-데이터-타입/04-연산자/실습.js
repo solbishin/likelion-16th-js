480c2a6c127028d3221aea96b21ce3de125dc8a1
solbishin <solbishin01@gmail.com>
2026-01-06 13:48:19 +0900

    학습: 할당 & 문자열 연결, 논리 및 비교 연산자 - OR(||) 연산자 - AND(&&) 연산자 - NOT(!) 연산자 - 이중 부정 (Boolean() 대체) - 숫자 비교 - 문자 비교

--- 자바스크립트/01-변수-데이터-타입/04-연산자/실습.js
+++ 자바스크립트/01-변수-데이터-타입/04-연산자/실습.js
@@ -150,25 +150,51 @@ console.log(count) // count = 5
 // [할당 연산] 점수 누적
 // --------------------------------------------------------------------------
 let totalScore = 0
-// console.log(totalScore)
+totalScore += 92
+totalScore += 100
+totalScore += 81
+console.log(totalScore)
 
 // [할당 연산] 재고 감소
 let stock = 100
-// console.log(stock)
+stock -= 5
+stock -= 15
+stock -= 34
+
+console.log(stock)
 
 // [할당 연산] 가격 인상
 let price = 10000
-// console.log(price)
+price *= (1 + 0.05) // 5% 인상
+
+console.log(price)
 
 // --------------------------------------------------------------------------
 // [산술 연산] 문자열 연결
 // --------------------------------------------------------------------------
 
 // 문자열 + 문자열
+const 성 = '지'
+const 이름 = '훈'
+console.log(성 + 이름)
 
 // 문자열 + 숫자 (숫자가 문자열로 변환됨)
+// 문자 + 숫자
+// 문자열 + '숫자' (문자열화) = 문자열
+const greetingMessage = '안녕!'
+const myFavoriteNumber = 9
+const result = greetingMessage + myFavoriteNumber
+// const result = '안녕!' + 9
+// const result = '안녕!' + '9'
+// const result = '안녕!9'
+
+console.log(result, typeof result)
+
 
 // 숫자 + 문자열
+Result = myFavoriteNumber + greetingMessage
+console.log(Result, typeof Result)
+
 
 
 // --------------------------------------------------------------------------
@@ -196,13 +222,24 @@ let price = 10000
 // 모든 조건이 true여야 true
 
 // 예제: 로그인 & 권한 검증
-let is_logged_in
-let has_permission
+//      Netflix 구독(계정 소유 && 사용료 지불)
+let is_logged_in = false
+let has_permission = false
 // 로그인 ❌ 권한 ❌ 출력 결과: false
-// 로그인 ✅ 권한 ❌ 출력 결과: false
-// 로그인 ❌ 권한 ✅ 출력 결과: false
-// 로그인 ✅ 권한 ✅ 출력 결과: true
+console.log(is_logged_in && has_permission)
 
+// 로그인 ✅ 권한 ❌ 출력 결과: false
+is_logged_in = true
+console.log(is_logged_in && has_permission)
+
+// 로그인 ❌ 권한 ✅ 출력 결과: false
+is_logged_in = false
+has_permission = true
+console.log(is_logged_in && has_permission)
+
+// 로그인 ✅ 권한 ✅ 출력 결과: true
+is_logged_in = true
+console.log(is_logged_in && has_permission)
 
 // --------------------------------------------------------------------------
 // [논리 연산] OR (||)
@@ -216,9 +253,22 @@ let has_permission
 let is_handsome
 let is_rich
 // 잘생김 ❌ 부자 ❌ 출력 결과: false
+is_handsome = false
+is_rich = false
+console.log(is_handsome || is_rich)
+
 // 잘생김 ❌ 부자 ✅ 출력 결과: true
+is_rich = true
+console.log(is_handsome || is_rich)
+
 // 잘생김 ✅ 부자 ❌ 출력 결과: true
+is_handsome = true
+is_rich = false
+console.log(is_handsome || is_rich)
+
 // 잘생김 ✅ 부자 ✅ 출력 결과: true
+is_rich = true
+console.log(is_handsome || is_rich)
 
 
 // --------------------------------------------------------------------------
@@ -229,43 +279,88 @@ let is_rich
 let truthy = true
 // 출력 결과: false
 let reverse_truthy
+console.log(reverse_truthy)
 
 let falsy = false
+let reverse_falsy = !falsy
 // 출력 결과: true
-let reverse_falsy
+console.log(reverse_falsy)
 
 // 예제: 로그인 상태 반전
+let is_logged_out = !is_logged_in
+
 // 출력 결과: is_logged_in 값 반전
-let is_logged_out
+console.log(is_logged_out)
 
 // 이중 부정 (!!) - boolean 변환 (Boolean() 대체)
 // '자바스크립트'
+console.log(Boolean('자바스크립트'))
+console.log(Boolean(!!'자바스크립트'))
+
 // '' 변환 출력 결과: false
+console.log(Boolean(''))
+console.log(Boolean(!!''))
+
 // ' ' 변환 출력 결과: true
+console.log(Boolean(' '))
+console.log(Boolean(!!''))
+
 // 0 변환 출력 결과: false
+console.log(Boolean(0))
+console.log(Boolean(!!0))
+
 // '0' 변환 출력 결과: true
+console.log(Boolean('0'))
+console.log(Boolean(!!'0'))
+
 // 1 변환 출력 결과: true
+console.log(Boolean(1))
+console.log(Boolean(!!1))
+
 // '1' 변환 출력 결과: true
+console.log(Boolean('1'))
+console.log(Boolean(!!'1'))
+
 // null 변환 출력 결과: false
+console.log(Boolean(null))
+console.log(Boolean(!!null))
+
 // undefined 변환 출력 결과: false
+console.log(Boolean(undefined))
+console.log(Boolean(!!undefined))
 
 
 // --------------------------------------------------------------------------
 // [비교 연산] 크기 비교
 // --------------------------------------------------------------------------
+const a = 10, b = 5
 
 // 숫자 비교
 // * 10이 5보다 큼
+console.log(a > b)
 // * 10이 5보다 작지 않음
+console.log(a < b)
 // * 10이 10보다 크거나 같음
+console.log(a >= b)
 // * 10이 5보다 작거나 같지 않음
+console.log(a <= b)
 
 // 문자열 비교 (사전순)
 // * 'a', 'b' 비교
 // * 'apple', 'banana' 비교
 // * 'A', 'a' 비교 (대문자가 소문자보다 작음)
 
+const memberName = '김이현'
+const partnerName = '이주인'
+
+console.log('a' > 'b')
+console.log('2' > 'k')
+
+
 // 문자열과 숫자 비교 (문자열이 숫자로 변환됨)
+console.log(memberName < partnerName)
+console.log('박아영' < '장현주')
+console.log('장현주' < '최수인')
 // * '10'이 5보다 큼
 // * '100'이 '20'보다 작음 (문자열끼리 비교하면 사전순!)
 
