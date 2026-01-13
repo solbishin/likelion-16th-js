49fb5ce5b4a05a73748f32683c4f1e781324231a
solbishin <solbishin01@gmail.com>
2026-01-13 15:50:17 +0900

    학습: 문서 객체 모델
    - 모델링이란?
    - 웹 페이지 제목 읽기/쓰기

--- 자바스크립트/01-변수-데이터-타입/15-브라우저-문서-객체-모델/learn.js
+++ 자바스크립트/01-변수-데이터-타입/15-브라우저-문서-객체-모델/learn.js
@@ -19,19 +19,19 @@
 // --------------------------------------------------------------------------
 
 // 1. 최상위(root) 객체인 window를 콘솔에 출력하세요.
-console.log(window) // Window { navigator, location }
+// console.log(window) // Window { navigator, location }
 
 // 2. window 객체의 navigator 속성을 통해 브라우저 정보를 출력하세요. (점 표기법 사용)
 
 // 객체의 속성에 접근
 // - 점(.) 표기법
-console.log(window.navigator.userAgent) // Navigator { userAgent }
+// console.log(window.navigator.userAgent) // Navigator { userAgent }
 // - 대괄호([]) 표기법
-console.log(window['navigator']['userAgent'])
+// console.log(window['navigator']['userAgent'])
 
 // 3. 위와 동일한 정보를 window를 생략하고 출력해 보세요. (전역 객체의 특징)
-console.log(navigator, typeof navigator) // 'object'
-console.log(navigator.userAgent, typeof navigator.userAgent) // 'string'
+// console.log(navigator, typeof navigator) // 'object'
+// console.log(navigator.userAgent, typeof navigator.userAgent) // 'string'
 
 
 // 설명:
@@ -44,33 +44,22 @@ console.log(navigator.userAgent, typeof navigator.userAgent) // 'string'
 // --------------------------------------------------------------------------
 
 // 1. 사용자에게 '유튜브 페이지로 이동할까요?'라고 묻는 
-//    확인창을 띄우고 결과를 변수 `userConfirm`에 담으세요.
-
-// console.log(window.confirm('유튜브 페이지로 이용할까요?'))
-
-// function myConfirm(message, confirmValue) {
-//   console.log(message, confirmValue)
-//   if (confirmValue) return true
-//   else return false
-// }
-
-// myConfirm('유튜브 페이지로 이동할까요?', true)
-// myConfirm('유튜브 페이지로 이동할까요?', false)
+//    확인창(confirm)을 띄우고 결과를 변수 `userConfirm`에 담으세요.
+// const moveToYoutube = window.confirm('유튜브 페이지로 이동할까요?')
+// console.log(moveToYoutube) // Boolean
 
 // 2. 조건문을 작성하세요.
-const moveToYoutube = window.confirm('유튜브 페이지로 이동할까요?')
+//    - [참일 때]: '네. 유튜브 페이지로 이동할게요!' 경고창(alert)을 띄우고,
+//      `window.location.href`를 'https://www.youtube.com'으로 변경하세요.
+// if (moveToYoutube) {
+// window.alert('네. 유튜브 페이지로 이동할게요!')
+// window.location.href = 'https://www.youtube.com'
+// }
+//    - [거짓일 때]: '네. 유튜브 페이지로 이동을 취소할게요!' 경고창을 띄우세요.
+// else {
+// window.alert('네. 유튜브 페이지로 이동을 취소할게요!')
+// }
 
-
-if (moveToYoutube) {
-  //    - [참일 때]: '네. 유튜브 페이지로 이동할게요!' 경고창(alert)을 띄우고,
-  //      `window.location.href`를 'https://www.youtube.com'으로 변경하세요.
-  window.alert('네, 유튜브 페이지로 이동할게요!')
-  window.location.href = 'https://www.youtube.com'
-}
-//    - [거짓일 때]: '페이지 이동을 취소했어요.' 경고창을 띄우세요.
-else {
-  window.alert('페이지 이동을 취소했어요!')
-}
 
 // 설명:
 // confirm()은 확인 시 true, 취소 시 false를 반환합니다.
@@ -82,12 +71,15 @@ else {
 // --------------------------------------------------------------------------
 
 // 1. 현재 로드된 문서를 나타내는 document 객체를 콘솔에 출력하세요.
-
+// console.log(window.document)
 
 // 2. document 객체를 객체 내부 구조(속성 등)까지 포함하여 자세히(dir) 출력하세요.
-
+// console.dir(document)
 
 // 3. document 객체와 window.document 객체가 동일한지 비교(===)한 결과를 출력하세요.
+// console.log(window.document === document) // 동일 객체인가?
+const doc = document, htmlDoc = document
+// console.log(doc === document, htmlDoc === document)
 
 
 // 설명:
@@ -124,7 +116,7 @@ else {
 // --------------------------------------------------------------------------
 
 // 1. 현재 페이지의 제목(title)을 가져와 '현재 페이지 제목' 문구와 함께 출력하세요.
-
+console.log('현재 웹 페이지의 제목:', document.title) // 웹 페이지 제목 읽기
 
 // 2. 현재 문서의 전체 URL 주소를 출력하세요.
 
@@ -137,7 +129,7 @@ else {
 
 // 설명:
 // DOM 객체의 속성에 새로운 값을 할당하면 실제 브라우저의 렌더링 상태가 즉시 변경됩니다.
-
+document.title = '멋사 FE.16 JavaScript 학습!' // 웹 페이지 제목 쓰기
 
 // --------------------------------------------------------------------------
 // 핵심 요약!
