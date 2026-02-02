21eac47cf3b0c983629cc9cf230e3318a6207431
solbishin <solbishin01@gmail.com>
2026-02-02 10:13:32 +0900

    리마인드: 예전 방법의 문자열 생성 함수 - 문자열 연결 - 템플릿처럼 이스케이트 처리 - 배열 → 문자열 조인(join)

--- 자바스크립트/01-변수-데이터-타입/48-탬플릿-리터럴/learn.js
+++ 자바스크립트/01-변수-데이터-타입/48-탬플릿-리터럴/learn.js
@@ -6,17 +6,71 @@
 // 1. 객체(product)를 만들고 이름과 가격을 담으세요.
 // 2. 백틱(``)을 사용하여 여러 줄로 구성된 HTML 마크업 문자열을 생성하세요.
 // 3. ${} 문법을 사용하여 상품 정보를 동적으로 삽입하세요.
-console.groupCollapsed('템플릿 리터럴 기본 실습')
+console.group('템플릿 리터럴 기본 실습')
 
-function generateHTMLCode(
-  data /* { id: '', label: '', type: '', placholder: '' } */,
-) {
+// 예전 방법의 문자열 생성 함수
+function generateOldHTMLCode(data) {
+
+  // 방법 1. 문자열 연결(접합)
+  let htmlCode = ''
+  htmlCode += '<div class="form-input">'
+  htmlCode += '<label for="' + data.id + '">' + data.label + '</label>'
+  htmlCode += '<input type="' + data.type + '" id="' + data.id + '" placeholder="' + data.placholder + '" />'
+  htmlCode += '</div>'
+
+  console.log('문자열 연결(접합) 결과\n', htmlCode)
+
+  // 방법 2. 템플릿 비슷하게 이스케이프 처리
+  htmlCode = '<div class="form-input">\
+    <label for="'+ data.id + '">' + data.label + '</label>\
+    <input\
+      type="'+ data.type + '"\
+      id="'+ data.id + '"\
+      placeholder="'+ data.placholder + '"\
+    />\
+  </div>'
+
+  console.log('템플릿 비슷하게 이스케이프 처리\n', htmlCode)
+
+
+  // 방법 3. 배열 사용해 문자열로 조인(join)
+  htmlCode = [
+    '<div class="form-input">',
+    '<label for="' + data.id + '">' + data.label + '</label>',
+    '<input',
+    ' type="' + data.type + '"',
+    ' id="' + data.id + '"',
+    ' placeholder="' + data.placholder + '"',
+    '/>',
+    '</div>',
+  ].join('')
+
+  console.log('배열 사용해 문자열로 조인(join)\n', htmlCode)
+
+  return htmlCode
+}
+
+console.log(
+  generateOldHTMLCode({
+    id: 'i@sodkckd-82',
+    label: '이름',
+    type: 'text',
+    placholder: '하월곡',
+  }),
+)
+
+// 템플릿 리터럴 방법을 사용한 문자열 생성 함수
+function generateHTMLCode(data) {
   // 템플릿(Template) 리터럴(Literal, 값)
   // JavaScript를 사용해 HTML 코드 구조화 (Markup)
   const htmlTemplate = `
     <div class="form-input">
       <label for="${data.id}">${data.label}</label>
-      <input type="${data.type}" id="${data.id}" placeholder="${data.placholder}" />
+      <input
+        type="${data.type}"
+        id="${data.id}"
+        placeholder="${data.placholder}"
+      />
     </div>
   `
 
@@ -48,14 +102,7 @@ console.log(emailInputTemaplte)
 console.log(passwordInputTemaplte)
 console.log(passwordConfirmInputTemaplte)
 
-// 생성할 HTML 코드
-// <div class="form-input">
-//   <label for="{아이디}">{레이블}</label>
-//   <input type="{타입}" id="{아이디}" placeholder="{플레이스홀더}" />
-// </div>
-
 console.groupEnd()
-
 
 // [실습] 템플릿 리터럴 내부의 연산
 // 1. 장바구니에 담긴 물건의 가격(price)과 수량(quantity) 변수를 선언하세요.
@@ -65,7 +112,6 @@ console.groupCollapsed('표현식 삽입 실습')
 // 이곳에 코드를 작성하세요
 
 console.groupEnd()
-
 
 // --------------------------------------------------------------------------
 // 실습: 태그드 템플릿 (Tagged Templates)
@@ -77,13 +123,26 @@ console.groupEnd()
 // 3. 변수 부분에만 <strong> 태그를 감싸서 반환하는 로직을 작성해 보세요.
 console.groupCollapsed('태그드 템플릿 심화 실습')
 
-// 이곳에 코드를 작성하세요
-function highlight(strings, ...values) {
+// 태그 함수 호출 (역따옴표 활용)
+highlight`
+  ${document.body}
+  font-weight: 700; 
+  ${document.querySelector('main')}
+  font-size: 20px;
+  ${document.querySelector('h1')}
+  background-color: #ff0;
+  ${document.querySelector('section')}
+`
+
+// 태그 함수
+function highlight(strings, body, main, h1, section) {
   // 힌트: strings 배열과 values 배열을 결합해 보세요
+  const elements = [body, main, h1, section]
+  elements.forEach((element) => {
+    element.style.cssText = strings.join('')
+  })
 }
-
 console.groupEnd()
-
 
 // --------------------------------------------------------------------------
 // 핵심 요약!
